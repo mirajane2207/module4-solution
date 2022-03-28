010b6fdbc19820cac8b325e8af823a1bb8c1b8ca
Oksana Chernenko <mirajane2207@gmail.com>
2022-03-28 22:20:18 +0300

    implement module 4

--- script.js
+++ script.js
@@ -29,10 +29,6 @@ WARNING!!! WARNING!!!
 
 */
 
-// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
 
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
